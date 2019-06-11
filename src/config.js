d4a635d200c29479443ef39a8bb70de0aac835e9
Stelian Corbut <stelian@cdsromania.ro>
2019-06-11 13:50:37 +0300

    low timing

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-let workingTime = process.env.NODE_ENV === 'testing' ? 0.1 * 60 : 20 * 60
-export const WORKING_TIME = workingTime
+export const WORKING_TIME = 0.1 * 60
 export const RESTING_TIME = 5 * 60
 export const KITTEN_TIME = 5
